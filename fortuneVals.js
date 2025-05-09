2faad5f4f33dfa0d0e2f575b5c90c710be555533
Zoe Cox <zoemcox16@gmail.com>
2025-05-09 16:29:13 +0100

    logic and html

---
+++ fortuneVals.js
@@ -0,0 +1,26 @@
+const responses = {
+  positive: {
+    business:
+      "You will amass large fortune in business ventures, and have huge financial success.",
+    farm: "You will find contentment in running a rural farm.",
+    creative:
+      "You will find find major success in your creative field and beome critically acclaimed in it.",
+    alone:
+      "You seek happiness alone and in the company of animals. Your futures takes you to a cottage in the forest with several cats or dogs of your choice.",
+    family:
+      "You find the love of your life and settle with them, having as many children as you wish.",
+  },
+  negative: {
+    business:
+      "You put the money into a dodgy offshore account and lose it, thus you are unsuccessful in your ventures.",
+    farm: "You purchase a rural farm to run, but are haunted by Biblical crop plagues and the farm fails.",
+    creative:
+      "You quit your day job to pursue your creative endeavours, but fail and take employment as a pre-school recorder teacher.",
+    alone:
+      "You try to live alone with a horde of your favourite animal but only manage to find an ancient, evil cat that scratches constantly.",
+    family:
+      "You find the love of your life, but they are afflicted with a habit you hate the most.",
+  },
+};
+
+export { responses };
---
+++ script.js
@@ -0,0 +1,97 @@
+import { responses } from "./fortuneVals.js";
+
+const mirrorContainer = document.querySelector("#mirror-container");
+const submitBtn = document.querySelector("#submit-button");
+const resetBtn = document.querySelector("#reset-button");
+
+const radioButtons = {
+  environmentRadios: document.getElementsByName("environment-option"),
+  trolleyRadios: document.getElementsByName("trolley-option"),
+  moneyRadios: document.getElementsByName("money-option"),
+  doorRadios: document.getElementsByName("door-option"),
+};
+
+const userChoice = {
+  badPoints: 0,
+  goodPoints: 0,
+  environmentType: "",
+};
+
+const fortuneDecider = (environmentAnswer) => {
+  if (userChoice.badPoints > userChoice.goodPoints) {
+    switch (environmentAnswer) {
+      case "alone":
+        mirrorContainer.innerHTML = responses.negative.alone;
+        break;
+      case "business":
+        mirrorContainer.innerHTML = responses.negative.business;
+        break;
+      case "creative":
+        mirrorContainer.innerHTML = responses.negative.creative;
+        break;
+      case "farm":
+        mirrorContainer.innerHTML = responses.negative.farm;
+        break;
+      case "family":
+        mirrorContainer.innerHTML = responses.negative.family;
+        break;
+    }
+  }
+  if (userChoice.goodPoints > userChoice.badPoints) {
+    switch (environmentAnswer) {
+      case "alone":
+        mirrorContainer.innerHTML = responses.positive.alone;
+        break;
+      case "business":
+        mirrorContainer.innerHTML = responses.positive.business;
+        break;
+      case "creative":
+        mirrorContainer.innerHTML = responses.positive.creative;
+        break;
+      case "farm":
+        mirrorContainer.innerHTML = responses.positive.farm;
+        break;
+      case "family":
+        mirrorContainer.innerHTML = responses.positive.family;
+        break;
+    }
+  }
+};
+submitBtn.addEventListener("click", () => {
+  for (let i = 0; i < radioButtons.environmentRadios.length; i++) {
+    if (radioButtons.environmentRadios[i].checked) {
+      userChoice.environmentType = radioButtons.environmentRadios[i].value;
+      console.log(userChoice.environmentType);
+    }
+  }
+  for (let j = 0; j < radioButtons.trolleyRadios.length; j++) {
+    if (radioButtons.trolleyRadios[j].checked) {
+      radioButtons.trolleyRadios[j].value === "return-trolley"
+        ? userChoice.goodPoints++
+        : userChoice.badPoints++;
+      console.log(userChoice.badPoints, userChoice.goodPoints);
+    }
+  }
+  for (let k = 0; k < radioButtons.moneyRadios.length; k++) {
+    if (radioButtons.moneyRadios[k].checked) {
+      radioButtons.moneyRadios[k].value === "give-back"
+        ? userChoice.goodPoints++
+        : userChoice.badPoints++;
+      console.log(userChoice.badPoints, userChoice.goodPoints);
+    }
+  }
+  for (let l = 0; l < radioButtons.doorRadios.length; l++) {
+    if (radioButtons.doorRadios[l].checked) {
+      radioButtons.doorRadios[l].value === "hold-open"
+        ? userChoice.goodPoints++
+        : userChoice.badPoints++;
+      console.log(userChoice.badPoints, userChoice.goodPoints);
+    }
+  }
+  fortuneDecider(userChoice.environmentType);
+  console.log("submit button clicked");
+});
+
+resetBtn.addEventListener("click", () => {
+  location.reload();
+});
